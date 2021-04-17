a26fc2112e091c9d3637fa8e5b7ddec268a2d98a
Oksana Kochenko <oksanakochenkoo@gmail.com>
2021-04-17 14:57:18 +0300

    Savin to ls deleted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 import App from './сomponents/App/App';
 import store from './redux/contacts/store';
 import './index.css';
@@ -10,10 +10,10 @@ console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store.store}>
-      <PersistGate loading={null} persistor={store.persistor}>
-        <App />
-      </PersistGate>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={store.persistor}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
